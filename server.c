e450a90a72bb50301b691f543dd72f2805401b38
Parzing <jettae@schroff.com>
2026-03-30 00:38:05 -0400

    server.c should be commited lol

--- server.c
+++ server.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <termios.h>
+
 #include <stdbool.h>
 
 #include "socket.h"
@@ -17,22 +17,29 @@
 	#define QUIT_MSG ("quit")
 #endif
 
+#define MAX_CLIENTS 20
+
 void cleanup(struct sockaddr_in *self, int listenfd);
 
 
 int main() {
     struct sockaddr_in *self = init_server_addr(PORT);
     int listenfd = set_up_server_socket(self, 5);
-	int file_descriptor = accept_connection(listenfd);
-	if (file_descriptor < 0) {
-		cleanup(self, listenfd);
-		exit(1);
-	}
+
+
+	int client_fds[MAX_CLIENTS];
+    for (int i = 0; i < MAX_CLIENTS; i++){
+		client_fds[i] = -1;
+	} 
 
 	char message[PACKET_SIZE];
 
-	while(strcmp(message, QUIT_MSG) != 0) {
-		message[0] = '\0';
+	while(1) {	
+		fd_set pipe_set;
+        FD_ZERO(&pipe_set);
+        FD_SET(listenfd, &pipe_set);
+		int max_fd = listenfd;
+
 		get_message(file_descriptor, message);
 		if (strlen(message) != 0){
 			printf("message: %s\n", message);
